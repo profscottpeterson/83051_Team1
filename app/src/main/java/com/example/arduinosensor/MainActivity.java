3890ea6ca35c8a38cb6350a46251ef0580ac0318
mmoya26 <mmoya18@icloud.com>
2019-11-05 22:59:21 -0600

    Test commit

--- app/src/main/java/com/example/arduinosensor/MainActivity.java
+++ app/src/main/java/com/example/arduinosensor/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference dataNode = mRootReference.child("data");
 
 
+    // Test comment
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
